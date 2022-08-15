dbb84d6d322ac361d36bbb3cedf994708e104155
acsa-laa <arcanjodagmar@gmail.com>
2022-08-15 16:27:51 -0300

    first commit

---
+++ src/main/java/br/ufrn/imd/models/BankAccount.java
@@ -0,0 +1,55 @@
+package br.ufrn.imd.models;
+
+import org.jetbrains.annotations.NotNull;
+
+public class BankAccount {
+    private long accountNumber;
+    private int agency;
+    private double balance;
+
+    public BankAccount() {
+        this.accountNumber = 0;
+        this.agency = 0;
+        this.balance = 0;
+    }
+
+    public BankAccount(long accountNumber, int agency, double balance) {
+        this.accountNumber = accountNumber;
+        this.agency = agency;
+        this.balance = balance;
+    }
+
+    public int getAgency() {
+        return agency;
+    }
+
+    public long getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double value) {
+        if (value <= 0) throw new IllegalArgumentException();
+        this.balance += value;
+    }
+
+    public void withdraw(double value){
+        if(value <= 0) throw new IllegalArgumentException();
+        else if (getBalance() < value) throw new ArithmeticException();
+        else{
+            this.balance = balance - value;
+        }
+    }
+
+    public void transfer(BankAccount beneficiaryAccount, double value){
+        if(value <= 0) throw new IllegalArgumentException();
+        else if (getBalance() < value) throw new ArithmeticException();
+        else{
+            this.balance = this.balance - value;
+            beneficiaryAccount.balance = beneficiaryAccount.balance + value;
+        }
+    }
+}
